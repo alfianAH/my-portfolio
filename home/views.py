52e2916a16d71a1c7e30ff9a9d36e896612977a0
alfianAH <alfian.hamdani04@gmail.com>
2022-02-16 21:01:57 +0800

    Add professional summary and about in home view

--- home/views.py
+++ home/views.py
@@ -1,3 +1,33 @@
+from django.http import Http404
 from django.shortcuts import render
 
-# Create your views here.
+from .models import (
+    About,
+    ProfessionalSummary,
+)
+
+
+def home_view(request):
+    """Make home view
+
+    Args:
+        request (): 
+
+    Returns:
+        any: Render page
+    """
+    professional_summary_qs = ProfessionalSummary.objects.all()
+    about_qs = About.objects.all()
+
+    if not professional_summary_qs.exists() or not about_qs.exists():
+        return Http404
+    
+    professional_summary = professional_summary_qs[0]
+    about = about_qs[0]
+    
+    context = {
+        'professional_summary': professional_summary,
+        'about': about
+    }
+
+    return render(request, 'home-view.html', context=context)
--- my_portfolio/views.py
+++ my_portfolio/views.py
@@ -1,17 +1,1 @@
-"""
-To render HTML web page
-"""
 
-from django.shortcuts import render
-
-
-def home_view(request):
-    """
-    Make home view
-    @param request: Django's request
-    @return: Returns HTML as a response
-    """
-    
-    # Django templates
-    html_fn = "home-view.html"
-    return render(request, html_fn)
