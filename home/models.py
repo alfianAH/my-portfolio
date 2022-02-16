d0e2ac3d5d1250c09747f3b156c3ad650b86ebe1
alfianAH <alfian.hamdani04@gmail.com>
2022-02-16 17:59:34 +0800

    Add professional summary and about model and test it

--- home/models.py
+++ home/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+from re import T
+from djongo import models
 
 # Create your models here.
+class ProfessionalSummary(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class About(models.Model):
+    description = models.TextField()
+    photo_url = models.CharField(max_length=200)
+    resume_url = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,53 @@
 from django.test import TestCase
 
+from .models import (
+    About,
+    ProfessionalSummary,
+)
+
 # Create your tests here.
+class HomeTestCase(TestCase):
+    def setUp(self):
+        # Create professional summary
+        self.professional_summary = ProfessionalSummary.objects.create(
+            title='Welcome and hello,',
+            content="I'm Alfian Aldy Hamdani. I'm currently an Informatics college student in Hasanuddin University. I love talking about Games and Machine Learning. I am eager to learn about new technologies."
+        )
+
+        # Create about
+        self.about = About.objects.create(
+            description="Test",
+            photo_url='https://google.com',
+            resume_url='https://google.com',
+        )
+
+    def test_count_professional_sumary_exists(self):
+        qs = ProfessionalSummary.objects.all()
+        self.assertTrue(qs.exists())
+
+    def test_count_about_exists(self):
+        qs = About.objects.all()
+        self.assertTrue(qs.exists())
+
+    def test_count_professional_summary(self):
+        qs = ProfessionalSummary.objects.all()
+        self.assertEqual(qs.count(), 1)
+
+    def test_count_about(self):
+        qs = About.objects.all()
+        self.assertEqual(qs.count(), 1)
+
+    def test_professional_summary_fields(self):
+        qs = ProfessionalSummary.objects.all()[0]
+        self.assertEqual(qs.title, 'Welcome and hello,')
+        print('Professional Summary')
+        print(qs.created)
+        print(qs.updated)
+
+    def test_about_fields(self):
+        qs = About.objects.all()[0]
+        self.assertEqual(qs.description, 'Test')
+        print('About')
+        print(qs.created)
+        print(qs.updated)
+
