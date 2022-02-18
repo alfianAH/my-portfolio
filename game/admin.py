ce37147a54ebcdb484be4e645f3887611f9b991c
alfianAH <alfian.hamdani04@gmail.com>
2022-02-18 11:50:11 +0800

    Add game models and admin

---
+++ game/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+
+from .models import (
+    GameCarousel,
+    GameDetail, 
+    GameProject
+)
+
+class GameCarouselInline(admin.StackedInline):
+    model = GameCarousel
+    extra = 0
+
+
+class GameDetailInline(admin.StackedInline):
+    model = GameDetail
+    extra = 0
+
+
+class GameProjectAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'created', 'updated']
+    readonly_fields = ['created', 'updated']
+    inlines = [GameCarouselInline, GameDetailInline]
+
+
+# Register your models here.
+admin.site.register(GameProject, GameProjectAdmin)
---
+++ game/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+from home.validators import validate_url
+
+# Create your models here.
+class GameStatus(models.TextChoices):
+    ON_GOING = 'o', 'On Going'
+    COMPLETED = 'c', 'Completed'
+
+
+class GameProject(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    play_url = models.CharField(max_length=200, validators=[validate_url])
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class GameDetail(models.Model):
+    project = models.ForeignKey(GameProject, on_delete=models.CASCADE)
+    status = models.CharField(max_length=1, choices=GameStatus.choices, default=GameStatus.COMPLETED)
+    platform = models.CharField(max_length=20)
+    genre = models.CharField(max_length=100)
+    engine = models.CharField(max_length=20)
+    language = models.CharField(max_length=10)
+    team = models.TextField()
+
+
+class GameCarousel(models.Model):
+    project = models.ForeignKey(GameProject, on_delete=models.CASCADE)
+    url = models.CharField(max_length=200, validators=[validate_url])
