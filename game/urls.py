61103c058a56ad613ee98cd9fb0b88a228bbdfba
alfianAH <alfian.hamdani04@gmail.com>
2022-02-24 15:15:10 +0800

    Add modal for project detail

--- game/urls.py
+++ game/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
 
-from .views import game_project_detail_view
-
-
 app_name = 'game'
 urlpatterns = [
     # path('create/', , name='create'),
-    path('<slug:slug>/', game_project_detail_view, name='detail'),
+    # path('<slug:slug>/', game_project_detail_view, name='detail'),
 ]
