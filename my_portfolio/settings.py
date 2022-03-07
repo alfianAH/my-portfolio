5b4b5860d396e594650d3d17a3c9c249dd86c02c
alfianAH <alfian.hamdani04@gmail.com>
2022-03-07 20:16:28 +0800

    Add gunicorn and static file folder

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -135,6 +135,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
+STATIC_ROOT = BASE_DIR / 'staticfiles-cdn'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
